0290452d669a7c72dccfabfc8194d111e726a2bc
gagandeepsingh21 <gagandeepgahir@gmail.com>
2021-02-23 15:20:19 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const Mainmenu = document.querySelector(".mynav_ul");
+const closemenu = document.querySelector(".closeMenu");
+const Openmenu = document.querySelector(".Openmenu");
+
+Openmenu.addEventListener('click',show);
+closemenu.addEventListener('click',close);
+function show(){
+    Mainmenu.style.display ='flex';
+    Mainmenu.style.top='0';
+
+}
+function close(){
+    Mainmenu.style.top='-100%';
+}
