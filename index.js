d88ea2983fc2e44174caf386670a93e251506b31
gagandeepsingh21 <gagandeepgahir@gmail.com>
2021-06-05 16:30:58 +0300

    changes

--- index.js
+++ index.js
@@ -12,4 +12,46 @@ function show(){
 }
 function close(){
     Mainmenu.style.top='-110%';
+    
 }
+// const observerOptions = {
+//   root: null,
+//   rootMargin: "100px",
+//   threshold: 0.7
+// };
+
+// function observerCallback(entries, observer) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       // fade in observed elements that are in view
+//       entry.target.classList.replace('fadeOut', 'faderight');
+//     } else {
+//       // fade out observed elements that are not in view
+//       entry.target.classList.replace('faderight', 'fadeOut');
+//     }
+//   });
+// }
+// const Fade = document.querySelectorAll('.fade');
+
+// const observer = new IntersectionObserver(observerCallback, observerOptions);
+// Fade.forEach(el => {observer.observe(el)});
+
+const obseverOption = {
+
+    root: null,
+    rootMargin: "100px",
+    threshold: 0.7
+};
+function observerCallback(entries, observer){
+    entries.forEach(entry =>{
+        if(entry.isIntersecting){
+            entry.target.classList.replace('fadeOut','faderight');
+        }else{
+            entry.target.classList.replace('faderight','fadeOut');
+        }
+    });
+}
+const observer = new IntersectionObserver(observerCallback, obseverOption)
+const Fade = document.querySelectorAll('.fade');
+Fade.forEach(a => {observer.observe(a)})
+
